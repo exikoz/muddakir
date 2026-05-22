a82b28476665b68e42cd4afcdf435b2b5f367010
exikoz <hussein.hasnawy@gmail.com>
2026-05-22 05:33:18 +0200

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './i18n/config'
 import './index.css'
 import './store/themeStore' // Initialize theme before render
@@ -8,5 +9,6 @@ import App from './app/App'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
