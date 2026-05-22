5fd01ae0607cfde605acef7eeecf3e67b1ac70c0
exikoz <hussein.hasnawy@gmail.com>
2026-05-22 05:35:47 +0200

    fix: use inject() for Vercel Analytics instead of /react subpath

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
+import { inject } from '@vercel/analytics'
 import './i18n/config'
 import './index.css'
 import './store/themeStore' // Initialize theme before render
 import App from './app/App'
 
+inject()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
